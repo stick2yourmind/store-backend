c1942068ce5d745943c35585f9472c04eaaf906f
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-02 01:07:52 -0300

    🔨 setup public folder

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
+  // Serve public folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(configService.getOrThrow('APP_PORT'));
 }
 bootstrap();
