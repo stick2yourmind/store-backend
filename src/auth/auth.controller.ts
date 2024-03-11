4730e20e850400eb2c29fce4141002958eee4888
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-11 00:54:40 -0300

    🔨 implement sign in service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 import { Controller, Post, Body, Res, UseInterceptors } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { SignInInput } from 'src/auth/dto/sign-in.input.dto';
 import { SignUpInput } from 'src/auth/dto/sign-up.input';
 import { PrismaInterceptor } from 'src/common/interceptor/prisma.interceptor';
 
@@ -8,6 +9,14 @@ import { PrismaInterceptor } from 'src/common/interceptor/prisma.interceptor';
 @UseInterceptors(PrismaInterceptor)
 export class AuthController {
   constructor(private readonly _authService: AuthService) {}
+
+  @Post('sign-in')
+  async signIn(@Res({ passthrough: true }) res: Response, @Body() signInInput: SignInInput) {
+    const user = await this._authService.signIn(signInInput);
+
+    this._authService.setCookie(res, user.token);
+    return user;
+  }
 
   @Post('sign-up')
   async signUp(@Res({ passthrough: true }) res: Response, @Body() signUpInput: SignUpInput) {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { Response } from 'express';
+import { SignInInput } from 'src/auth/dto/sign-in.input.dto';
 import { SignUpInput } from 'src/auth/dto/sign-up.input';
 import { UserService } from 'src/core/user/user.service';
 
@@ -13,6 +14,24 @@ export class AuthService {
     private readonly _jwtService: JwtService,
     private readonly _userService: UserService,
   ) {}
+
+  async signIn({ email, password }: SignInInput) {
+    const user = await this._userService.findByEmail(email);
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Invalid password or email');
+    }
+
+    const token = this._jwtService.sign({ id: user.id });
+    return {
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    };
+  }
 
   async signUp(signUpInput: SignUpInput) {
     const passwordHash = bcrypt.hashSync(
@@ -39,7 +58,7 @@ export class AuthService {
   setCookie(res: Response, token: string) {
     res.cookie('userToken', token, {
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-      httpOnly: this._configService.getOrThrow('NODE_ENV') === 'PROD',
+      httpOnly: this._configService.getOrThrow('NODE_END') === 'PROD',
       secure: true,
       sameSite: 'none',
     });
