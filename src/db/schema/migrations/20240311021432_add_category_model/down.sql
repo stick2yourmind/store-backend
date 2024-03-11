ee4a17f86389f9434fbfe6f3034e733d52b94a33
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-10 23:15:29 -0300

    🗃️ add category model to database model

---
+++ src/db/schema/migrations/20240311021432_add_category_model/down.sql
@@ -0,0 +1,12 @@
+-- DropForeignKey
+ALTER TABLE "_CategoryToProduct" DROP CONSTRAINT "_CategoryToProduct_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_CategoryToProduct" DROP CONSTRAINT "_CategoryToProduct_B_fkey";
+
+-- DropTable
+DROP TABLE "Category";
+
+-- DropTable
+DROP TABLE "_CategoryToProduct";
+
