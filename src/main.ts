b3b6d48a58684d53abcf33799c4fc00fd86ae950
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-22 00:31:52 -0300

    🔧 add logger response interceptor

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from 'src/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { LoggerInterceptor } from 'src/common/interceptor/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true });
   const configService = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalInterceptors(new LoggerInterceptor());
 
   // Add cookie middleware
   app.use(cookieParser());
