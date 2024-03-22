789597566b1fac337b81e0bbd04a9454ce1ad9a8
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-22 00:30:59 -0300

    🔧 enable raw request body

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true });
   const configService = app.get<ConfigService>(ConfigService);
 
   // Add cookie middleware
